633bf197cb61b6043f43868f9b9744a58f795e5e
Ccarretero <carreteroestudios@gmail.com>
2026-04-05 00:16:11 -0500

    Cambio en CORS para backend desplegado

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     RESET_TOKEN_EXPIRE_MINUTES: int = 30
-    BACKEND_CORS_ORIGINS: list[str] = ["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5500", "http://localhost:5500"]  # React/Vite/LiveServer
+    BACKEND_CORS_ORIGINS: list[str] = ["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5500", "http://localhost:5500", "https://front-colficultor.onrender.com"]  # React/Vite/LiveServer
     SMTP_HOST: str = "smtp.gmail.com"
     SMTP_PORT: int = 587
     SMTP_USER: str = ""
